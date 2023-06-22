a2a12d24b1a53053f35ca0b6a4161b703312a97d
LiamSirBean <rfwrl13@gmail.com>
2023-06-22 17:18:09 -0400

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,1 +1,9 @@
-window.open('about:blank', '_blank');
+window.location.href = 'about:blank';
+setTimeout(() => {
+  var iframe = document.createElement('iframe');
+  iframe.src = 'https://www.yourwebsite.com'; // Replace with your website URL
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  document.body.appendChild(iframe);
+}, 1000); // Delay of 1 second to ensure about:blank is loaded first
