0992efd9ac02ce96d882e14ca02a706faf71e87c
LiamSirBean <rfwrl13@gmail.com>
2023-06-22 17:23:33 -0400

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,46 +1,37 @@
-const checkPopupClosed = (popup) => {
-  return new Promise((resolve) => {
-    const checkClosedInterval = setInterval(() => {
-      if (popup.closed) {
-        clearInterval(checkClosedInterval);
-        resolve();
-      }
-    }, 500);
-  });
-};
+let inFrame
 
-const openPopup = () => {
-  const popup = window.open("about:blank", "_blank");
-  if (!popup || popup.closed) {
-    alert("Allow popups and redirects to hide this from showing up in your history.");
-  } else {
-    const doc = popup.document;
-    const iframe = doc.createElement("iframe");
-    const style = iframe.style;
-    const link = doc.createElement("link");
+try {
+    inFrame = window !== top
+} catch (e) {
+    inFrame = true
+}
 
-    const name = localStorage.getItem("name") || "My Drive - Google Drive";
-    const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
+// Cloaking Code
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) {
+        alert("Allow popups and redirects to hide this from showing up in your history.")
+    } else {
+        const doc = popup.document
+        const iframe = doc.createElement("iframe")
+        const style = iframe.style
+        const link = doc.createElement("link")
 
-    doc.title = name;
-    link.rel = "icon";
-    link.href = icon;
+        const name = localStorage.getItem("name") || "My Drive - Google Drive";
+        const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
 
-    iframe.src = location.href;
-    style.position = "fixed";
-    style.top = style.bottom = style.left = style.right = 0;
-    style.border = style.outline = "none";
-    style.width = style.height = "100%";
+        doc.title = name;
+        link.rel = "icon";
+        link.href = icon;
 
-    doc.head.appendChild(link);
-    doc.body.appendChild(iframe);
+        iframe.src = location.href
+        style.position = "fixed"
+        style.top = style.bottom = style.left = style.right = 0
+        style.border = style.outline = "none"
+        style.width = style.height = "100%"
 
-    checkPopupClosed(popup).then(() => {
-      openPopup();
-    });
-  }
-};
-
-if (window !== top && !navigator.userAgent.includes("Firefox")) {
-  openPopup();
+        doc.head.appendChild(link);
+        doc.body.appendChild(iframe)
+        self.close();
+    }
 }
