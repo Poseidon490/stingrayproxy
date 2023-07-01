173457d896ff09dce29f03f2327ba0fb2be33f9b
LiamSirBean <122816237+LiamSirBean@users.noreply.github.com>
2023-07-01 08:48:36 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tabs.html',
-    bare: 'https://interstellars.games',
+    bare: 'https://interstellars.games/bare',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
