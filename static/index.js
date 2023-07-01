0e12178a6c64e4a4c7a68b17c6d57d87c5fb0354
LiamSirBean <rfwrl13@gmail.com>
2023-07-01 08:21:11 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,32 +1,48 @@
-function isUrl(val = "") {
-    if (
-      /^http(s?):\/\//.test(val) ||
-      (val.includes(".") && val.substr(0, 1) !== " ")
-    )
-      return true;
-    return false;
-  }
-  
-  const form = document.querySelector("form");
-  const input = document.querySelector("input");
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
   
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
-    window.navigator.serviceWorker
-      .register("./sw.js", {
-        scope: __uv$config.prefix,
-      })
-      .then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) {
-          url = "https://www.google.com" + url;
-          location.href = __uv$config.prefix + __uv$config.encodeUrl(url)
-        }
-        else if (!(url.startsWith("https://") || url.startsWith("http://"))) {
-          url = "https://" + url;
-          location.href = __uv$config.prefix + __uv$config.encodeUrl(url)
-        }
-      });
+    try {
+        await registerSW();
+      } catch (err) {
+        error.textContent = "Failed to register service worker.";
+        errorCode.textContent = err.toString();
+        throw err;
+      }
+    
+      const url = search(address.value, searchEngine.value);
+      location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
   });
   
+  async function launchURL(openURL) {
+    try {
+      await registerSW();
+    } catch (err) {
+      error.textContent = "Failed to register service worker.";
+      errorCode.textContent = err.toString();
+      throw err;
+    }
   
+    const url = search(openURL, searchEngine.value);
+    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  }
