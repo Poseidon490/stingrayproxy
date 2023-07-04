1eff34a7148549a57fa062d671eb39468b77059f
Poseidon490 <60122384+Poseidon490@users.noreply.github.com>
2023-07-04 12:52:10 -0400

    Update tabs.js

--- static/assets/js/tabs.js
+++ static/assets/js/tabs.js
@@ -1,3 +1,5 @@
+// tabs.js
+
 let tabCount = 2; // Start tab count from 2 since home is initially loaded as tab 1
 
 function openTab(event, tabId) {
@@ -8,18 +10,18 @@ function openTab(event, tabId) {
 
   const tabLinks = document.getElementsByClassName('tab');
   for (let i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].classList.remove('active');
+    tabLinks[i].className = tabLinks[i].className.replace(' active', '');
   }
 
   document.getElementById(tabId).style.display = 'block';
-  event.currentTarget.classList.add('active');
+  event.currentTarget.className += ' active';
 }
 
 function addNewTab() {
   const tabList = document.getElementById('sortable');
   const tabButton = document.createElement('button');
   const newTabId = `tab${tabCount}`;
-  tabButton.className = 'tab active'; // Assign 'active' class to the new tab
+  tabButton.className = 'tab';
   tabButton.setAttribute('onclick', `openTab(event, '${newTabId}')`);
   tabButton.innerHTML = `Tab ${tabCount} <span class="close-button" onclick="closeTab(event)"><i class="fas fa-times"></i></span>`;
   tabList.appendChild(tabButton);
@@ -27,31 +29,33 @@ function addNewTab() {
   const tabPanel = document.createElement('div');
   tabPanel.className = 'tab-panel';
   tabPanel.id = newTabId;
-  tabPanel.style.height = '100%'; // Set parent height to 100%
-  const iframe = document.createElement('iframe');
-  iframe.src = '/static/tabs.html';
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-  iframe.style.border = 'none';
-  tabPanel.appendChild(iframe);
-  iframe.addEventListener('load', () => {
-    const title = iframe.contentDocument.title;
-    tabButton.innerHTML = title + ` <span class="close-button" onclick="closeTab(event)"><i class="fas fa-times"></i></span>`;
-  });
+  tabPanel.innerHTML = `
+  <html>
+<head>
+  <style>
+    body{
+        margin: 0; 
+    }
+    iframe{      
+        display: block;   
+        height: 100vh;   
+        width: 100vw;   
+        border: none; 
+        background: lightyellow; 
+    }
+</style>
+</head>
+<body>
+<iframe src="/static/tab.html" allowtransparency="true" scrolling="no" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+</body>
+</html>
+  `;
   document.querySelector('.tab-content').appendChild(tabPanel);
-
-  // Display the new tab's panel and hide others
-  const tabContent = document.getElementsByClassName('tab-panel');
-  for (let i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = 'none';
-  }
-  tabPanel.style.display = 'block';
 
   tabCount++;
 }
 
 function closeTab(event) {
-  event.stopPropagation();
   const tabButton = event.target.closest('.tab');
   const tabId = tabButton.getAttribute('onclick').match(/'([^']+)'/)[1];
 
@@ -65,3 +69,6 @@ function closeTab(event) {
 document.getElementById('tab1').addEventListener('click', (event) => {
   openTab(event, 'tab1');
 });
+
+// Event listener for the "Add Tab" button
+document.querySelector('.add-tab-button').addEventListener('click', addNewTab);
