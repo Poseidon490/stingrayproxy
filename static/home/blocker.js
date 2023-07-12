f5098a29c18085dc3edeb61ef4c0446d4aa10ebc
Poseidon490 <60122384+Poseidon490@users.noreply.github.com>
2023-07-12 10:31:20 -0400

    Update blocker.js

--- static/home/blocker.js
+++ static/home/blocker.js
@@ -1,12 +1,14 @@
-// blocker.js
+// Get the parent window and document
+var parentWindow = window.parent;
+var parentDocument = parentWindow.document;
 
-// Add a button to the page
-var button = document.createElement('button');
+// Create the button in the parent document
+var button = parentDocument.createElement('button');
 button.innerText = 'Block Element';
 button.style.position = 'fixed';
 button.style.top = '10px';
 button.style.right = '10px';
-document.body.appendChild(button);
+parentDocument.body.appendChild(button);
 
 // Function to block elements
 function blockElement(event) {
@@ -18,7 +20,7 @@ function blockElement(event) {
 // Add click event listener to the button
 button.addEventListener('click', function() {
   // Add click event listeners to all elements on the page
-  var elements = document.querySelectorAll('*');
+  var elements = parentDocument.querySelectorAll('*');
   for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', blockElement, true);
   }
