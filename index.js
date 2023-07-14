d57785bf1d31542fc6d8021abb4127e6c27880f3
Poseidon490 <60122384+Poseidon490@users.noreply.github.com>
2023-07-14 11:35:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,74 @@
-import express from "express";
-import http from "node:http";
 import { createBareServer } from "@tomphttp/bare-server-node";
+import express from "express";
+import { createServer } from "node:http";
+import { publicPath } from "ultraviolet-static";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import path from "node:path";
-import * as dotenv from "dotenv";
-dotenv.config();
+import { join } from "node:path";
+import { hostname } from "node:os";
 
-const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
-const bareServer = createBareServer("/bare/");
+const bare = createBareServer("/bare/");
+const app = express();
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
 app.use("/uv/", express.static(uvPath));
-app.use(express.static(path.join(__dirname, "static")));
 
-// Bare Server 
+// Error for everything else
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
+});
+
+const server = createServer();
+
 server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
   } else {
     app(req, res);
   }
 });
 
 server.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
 });
 
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
 server.on("listening", () => {
-  console.log(`Running at http://localhost:${process.env.PORT}`);
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
 });
 
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
+}
+
 server.listen({
-  port: process.env.PORT,
+  port,
 });
