698bd6636b5960beb2816faa3978438d43e538fc
Poseidon490 <60122384+Poseidon490@users.noreply.github.com>
2023-07-19 16:41:20 -0400

    Update uv.config.js

--- static/home/uv/uv.config.js
+++ static/home/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: 'https://use.astroid.wtf/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/home/uv-bacxkup/uv.handler.js',
-    bundle: '/static/home/uv-bacxkup/uv.bundle.js',
-    config: '/static/home/uv-bacxkup/uv.config.js',
+    handler: '/static/home/uv/uv.handler.js',
+    bundle: '/static/home/uv/uv.bundle.js',
+    config: '/static/home/uv/uv.config.js',
     sw: '/static/uv-sw.js',
 };
