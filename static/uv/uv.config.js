2fcf800a40a69602af012d6eb41a5c990e43729d
LiamSirBean <122816237+LiamSirBean@users.noreply.github.com>
2023-07-24 11:45:14 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
-    bare: 'https://tiw.tiwweb.workers.dev/',
+    prefix: '/static/',
+    bare: 'https://bare.starttiw.org/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
