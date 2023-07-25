96c961a9a11c6fb938ee1588ba644ff2809aca16
LiamSirBean <rfwrl13@gmail.com>
2023-07-25 13:37:27 -0400

    Meh

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/static/',
+    bare: 'https://bare.starttiw.org/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+}; 
